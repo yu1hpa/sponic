afd038d301fc0bc091071477a6266003cf0ec10b
yu1hpa <yu.1hpa@gmail.com>
2022-03-31 14:53:04 +0900

    feat: Fixed to be able to take songs from albums

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,17 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor } from './token-interceptor';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/token-interceptor.ts
+++ src/app/token-interceptor.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@angular/core';
 import {
-  HTTP_INTERCEPTORS,
   HttpEvent,
   HttpHandler,
   HttpRequest,
   HttpInterceptor,
-  HttpClient,
-  HttpHeaders
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  accessToken = '';
+  accessToken = 'BQCPb-ZP2jmLPcpFbifLX5vu3-9AN_BTAmINoVMGO2utKCsI4DJ-Vk4ElwqKD1dmf9Srp0oCRSKQnW359n0';
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const headers = req.headers.append('Authorization', `Bearer ${this.accessToken}`);
     const newRequest = req.clone({ headers });
@@ -20,6 +17,3 @@ export class TokenInterceptor implements HttpInterceptor {
     return next.handle(newRequest);
   }
 }
-export const httpInterceptorProviders = [
-  { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true},
-];
